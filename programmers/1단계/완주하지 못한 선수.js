d8e5179d491c22ed79886e341c95c82b55a2e526
Jeyoon <77595962+jeyoon2@users.noreply.github.com>
2021-09-09 23:47:27 +0900

    JavaScript

--- programmers/1단계/완주하지 못한 선수.js
+++ programmers/1단계/완주하지 못한 선수.js
@@ -1,13 +1,4 @@
-### 문제 설명
-
-수많은 마라톤 선수들이 마라톤에 참여하였습니다. \
-단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다. \
-마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, \
-완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
-
-### Codes (ver. 효율성 fail)
-
-```js
+// (ver. 효율성 not good)
 function solution(participant, completion) {
     var answer = '';
     // participant 배열의 요소에 대해 
@@ -32,10 +23,10 @@ function solution(participant, completion) {
     }
     return answer;
 }
-```
 
-### Codes (ver. 효율성 good)
-```js
+
+// (ver. 효율성 good)
+
 function solution(participant, completion) {
     var answer = '';
     participant.sort(); // 각 배열을 정렬
@@ -48,7 +39,3 @@ function solution(participant, completion) {
     }
    return answer;
 }
-```
-
-### 문제 url
-https://programmers.co.kr/learn/courses/30/lessons/4%E3%84%B9576
