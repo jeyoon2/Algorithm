1e290841863f1fe4da58dcc7642fbed4ca682320
Jeyoon <77595962+jeyoon2@users.noreply.github.com>
2021-09-10 23:16:13 +0900

    JavaScript.js

---
+++ programmers/1단계/K번째수.js
@@ -0,0 +1,29 @@
+
+// (반복문 이용)
+function solution(array, commands) {
+
+  let result = [];
+  
+    for (let i = 0; i < commands.length; i++) {
+        
+        let newArr = (array.slice(commands[i][0] - 1, commands[i][1])).sort(function(a,b) {
+            return a-b;
+        });
+        result.push(newArr[commands[i][2] - 1]);
+        }
+    return result;
+}
+
+// (map 함수 이용)
+function solution(array, commands) {
+// map 함수를 이용해 배열 commands를 바꾸어 리턴해준다
+
+    return commands.map(function(arr) {
+        
+        return ((array.slice(arr[0] - 1, arr[1])).sort(function(a, b) {
+            return a - b;
+        }))[arr[2] - 1];
+        
+    });
+}
+
