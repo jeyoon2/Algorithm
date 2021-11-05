eccfe06570d39260fd2218383c4f6eb768c4ebe6
Jeyoon <77595962+jeyoon2@users.noreply.github.com>
2021-11-05 10:58:29 +0900

    JavaScript

---
+++ basic/배열 탐색/4. 점수 계산.js
@@ -0,0 +1,14 @@
+function solution(arr) {
+  let answer = 0, cnt = 0;
+  // 누적되는 값의 지표인 cnt를 설정해줌
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      cnt = 0; 
+    } else {
+      cnt++; 
+      answer += cnt;
+    }
+  } 
+  return answer;
+}
