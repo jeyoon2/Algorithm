13d7933bebdc41a4f8c0ef9512a84042d87fe686
Jeyoon <77595962+jeyoon2@users.noreply.github.com>
2023-06-09 21:42:19 +0900

    가장 가까운 같은 글자.js

---
+++ programmers/1단계/가장 가까운 같은 글자.js
@@ -0,0 +1,26 @@
+function solution(s) {
+  let answer = [-1];
+
+  for (let i = 1; i < s.length; i++) {
+    let forwards = s.slice(0, i);
+
+    if (!forwards.includes(s[i])) {
+      answer.push(-1);
+      continue;
+    } else {
+      let result = Number.MAX_SAFE_INTEGER;
+
+      for (let j = 0; j < forwards.length; j++) {
+        if (s[i] === s[j]) {
+          if (result > i - j) {
+            result = i - j;
+          }
+        } else {
+          continue;
+        }
+      }
+      answer.push(result);
+    }
+  }
+  return answer;
+}
