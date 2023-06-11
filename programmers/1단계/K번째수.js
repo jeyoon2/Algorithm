5f82600a4e61b638d5076cc1e5db237f3925abe6
Jeyoon <77595962+jeyoon2@users.noreply.github.com>
2023-06-11 15:50:47 +0900

    JavaScript

--- programmers/1단계/K번째수.js
+++ programmers/1단계/K번째수.js
@@ -17,7 +17,6 @@ function solution(array, commands) {
 // (map 함수 이용)
 function solution(array, commands) {
 // map 함수를 이용해 배열 commands를 바꾸어 리턴해준다
-
     return commands.map(function(arr) {
         
         return ((array.slice(arr[0] - 1, arr[1])).sort(function(a, b) {
